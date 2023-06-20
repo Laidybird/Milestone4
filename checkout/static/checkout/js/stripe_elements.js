adfed890100d73c52c1f630fecaa11e664250740
Laidybird <predaveronica72@yahoo.com>
2023-06-20 08:17:25 +0000

    Added checkout success functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,83 +1,78 @@
-var stripe_public_key = $('#id_stripe_public_key').text();
-var client_secret = $('#id_client_secret').text();
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text();
+var clientSecret = $('#id_client_secret').text();
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
+
+// Create a style object for the card element
 var style = {
-    base: {
-        color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
-        }
-    },
-    invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
+  base: {
+    color: '#000',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
     }
+  },
+  invalid: {
+    color: '#dc3545',
+    iconColor: '#dc3545'
+  }
 };
-var card = elements.create('card', {style: style});
+
+// Create a card element
+var card = elements.create('card', { style: style });
+
+// Mount the card element to the container with ID "card-element"
 card.mount('#card-element');
 
-card.addEventListener('change', function(event) {
-    var cardErrors = document.getElementById('card-errors');
-    if (event.error) {
-        cardErrors.textContent = event.error.message;
-    } else {
-        cardErrors.textContent = '';
-    }
-});
-
-var form = document.getElementById('payment-form');
-
-// Get the card element
-const cardElement = elements.create('card');
-
-// Mount the card element
-cardElement.mount('#card-element');
-
 // Handle card validation errors
-cardElement.addEventListener('change', function (event) {
-    const cardErrors = document.getElementById('card-errors');
-    if (event.error) {
-        cardErrors.textContent = event.error.message;
-    } else {
-        cardErrors.textContent = '';
-    }
+card.addEventListener('change', function(event) {
+  var cardErrors = document.getElementById('card-errors');
+  if (event.error) {
+    cardErrors.textContent = event.error.message;
+  } else {
+    cardErrors.textContent = '';
+  }
 });
 
-form.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-    card.update({ 'disabled': true });
-    $('#submit-button').attr('disabled', true);
+// Handle form submission
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
 
-    // Create a payment method using the card element
-    stripe.createPaymentMethod({
-        type: 'card',
-        card: card,
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            errorDiv.textContent = result.error.message;
-            card.update({ 'disabled': false });
-            $('#submit-button').attr('disabled', false);
+  // Disable the submit button
+  document.getElementById('submit-button').disabled = true;
+
+  // Create a payment method using the card element
+  stripe.createPaymentMethod({
+    type: 'card',
+    card: card
+  }).then(function(result) {
+    if (result.error) {
+      // Display an error message
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+
+      // Enable the submit button
+      document.getElementById('submit-button').disabled = false;
+    } else {
+      // Payment method created successfully, proceed with payment confirmation
+      stripe.confirmCardPayment(clientSecret, {
+        payment_method: result.paymentMethod.id
+      }).then(function(confirmResult) {
+        if (confirmResult.error) {
+          // Payment confirmation failed, display an error message
+          var errorElement = document.getElementById('card-errors');
+          errorElement.textContent = confirmResult.error.message;
+
+          // Enable the submit button
+          document.getElementById('submit-button').disabled = false;
         } else {
-            // Payment method created successfully, proceed with payment confirmation
-            stripe.confirmCardPayment(client_secret, {
-                payment_method: result.paymentMethod.id
-            }).then(function(confirmResult) {
-                if (confirmResult.error) {
-                    // Payment confirmation failed
-                    var errorDiv = document.getElementById('card-errors');
-                    errorDiv.textContent = confirmResult.error.message;
-                    card.update({ 'disabled': false });
-                    $('#submit-button').attr('disabled', false);
-                } else {
-                    // Payment confirmation succeeded, submit the form
-                    form.submit();
-                }
-            });
+          // Payment confirmation succeeded, submit the form
+          form.submit();
         }
-    });
+      });
+    }
+  });
 });
