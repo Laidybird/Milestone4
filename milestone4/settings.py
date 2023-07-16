ec51728dadf7dd71065b66b290029a94b29c8223
Laidybird <predaveronica72@yahoo.com>
2023-07-16 17:46:51 +0000

    Remove secret key and debug

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w6&b(t(pwjf)1nf9l5(rp2-at&v(8s%g%g)09=o(0z$a1gr+6!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-laidybird-milestone4-z396znx4ip3.ws-eu101.gitpod.io']
 
