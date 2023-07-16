dde6f5812d559d282755f3949fa0ee9043f06d14
Laidybird <predaveronica72@yahoo.com>
2023-07-16 18:18:37 +0000

    Update the host

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-laidybird-milestone4-z396znx4ip3.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ['mugs-and-pillows-mania-4ab38e9b53bf.herokuapp.com']
 
 
 # Application definition
