f1bdab5258282fccf799bd127db00ca400451620
Laidybird <predaveronica72@yahoo.com>
2023-07-18 10:06:25 +0000

    Add aws and static settings

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     
 
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,27 @@ MEDIA_URL = '/media/'
 WSGI_APPLICATION = 'milestone4.wsgi.application'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# bucket config 
+
+if 'USE-AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'mugs-and-pillows-mania'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}'.s3.amazonaws.com
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
