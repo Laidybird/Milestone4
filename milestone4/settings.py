8ca1eada02eaa0476970d521ede799972d4cc26b
Laidybird <predaveronica72@yahoo.com>
2023-07-19 17:41:13 +0000

    Added django storage

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -182,6 +182,7 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.FileSystemStorage'
 
 
 MEDIA_URL = '/media/'
