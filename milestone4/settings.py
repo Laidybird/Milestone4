e3e74b6a8b97403ae14920a59ebbac783bb88f9e
Laidybird <predaveronica72@yahoo.com>
2023-07-19 17:24:53 +0000

    Fix an error

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -183,11 +183,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MIDDLEWARE = [
-    # ...
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    # ...
-]
+
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
