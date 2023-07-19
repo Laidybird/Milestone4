ab7968ab6a3762715a3ee89b8549b27ac3fefd72
Laidybird <predaveronica72@yahoo.com>
2023-07-19 08:21:53 +0000

    Fixing a typo in settings

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -123,25 +123,6 @@ WSGI_APPLICATION = 'milestone4.wsgi.application'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# bucket config 
-
-if 'USE-AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'mugs-and-pillows-mania'
-    AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}'.s3.amazonaws.com
-
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
-
-    # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -200,6 +181,27 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# bucket config 
+
+if 'USE-AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'mugs-and-pillows-mania'
+    AWS_S3_REGION_NAME = 'EU(London)eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    
 
 
 FREE_DELIVERY_THRESHOLD = 50
